788216b848606f9d9eff694329f0922c86efaa09
Mike <>
2020-11-01 01:45:21 +0800

    init

---
+++ BakuSnake/BakuSnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakuSnakeApp.swift
+//  BakuSnake
+//
+//  Created by 陳昱宏 on 2020/10/31.
+//
+
+import SwiftUI
+
+@main
+struct BakuSnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
